ae22a7ed7f538fd1ba38a560161285256a9835cd
Martyn Davies <martyn@orbit.love>
2021-06-03 20:16:09 +0200

    update test for new payload when adding content

--- src/github/orbit-action.test.js
+++ src/github/orbit-action.test.js
@@ -211,10 +211,10 @@ test("should create content for existing members", async () => {
     expect.stringContaining("/my-workspace/members/phacks/activities"),
     expect.objectContaining({
       body: JSON.stringify({
+        activity_type: "post",
         link: "https://github.com/orbit-love/orbit-model/issues/10#issuecomment-590037251",
-        title: "Commented on a GitHub issue",
-        description: "Commented on a GitHub issue",
-        key: "issue_comment:created", 
+        title: "Commented on GitHub",
+        description: "Commented on GitHub",
         occurred_at: "2020-02-23T07:55:28Z"
       }),
     })
