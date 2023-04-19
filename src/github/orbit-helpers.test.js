f008f0e8ffa4063b2a57a03b4a86cac4e1626aed
delete-44 <delete44+tech@protonmail.com>
2023-04-19 12:02:48 +0100

    Removed unneeded function to filter repositories

--- src/github/orbit-helpers.test.js
+++ src/github/orbit-helpers.test.js
@@ -1,9 +1,4 @@
-import testData from "./testData/activities.json";
-
-import {
-  _getRepositoryFullName,
-  _filterActivitiesByRepo,
-} from "./orbit-helpers";
+import { _getRepositoryFullName } from "./orbit-helpers";
 
 test("_getRepositoryFullName should return the full name of the repository based on window.location.pathname", () => {
   global.window = Object.create(window);
@@ -14,23 +9,4 @@ test("_getRepositoryFullName should return the full name of the repository based
     },
   });
   expect(_getRepositoryFullName()).toBe("hzoo/contributors-on-github");
-});
-
-test("_filterActivitiesByRepo should filter activities by repository", () => {
-  const activities = testData.data;
-  const included = testData.included;
-  const repositoryFullName = "theodo/falco";
-
-  expect(activities.length).toBe(3);
-  const filteredActivities = _filterActivitiesByRepo(
-    activities,
-    included,
-    repositoryFullName
-  );
-  expect(filteredActivities.length).toBe(2);
-  expect(
-    filteredActivities.map(
-      (activity) => activity.relationships.repository.data.id
-    )
-  ).toEqual(["210", "210"]);
 });
