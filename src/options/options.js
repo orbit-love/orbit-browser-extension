b0d6c3dc93c196a70b1cf99371377c1eab576f34
delete-44 <delete44+tech@protonmail.com>
2023-04-20 14:51:35 +0100

    [WIP] storing repsoitories in chunked blocks

--- src/options/options.js
+++ src/options/options.js
@@ -37,6 +37,7 @@ window.orbit = () => ({
         const { data, included } = await response.json();
         workspaces = data;
         repositories = included.filter((item) => item.type === "repository");
+        console.log(repositories);
       } catch (err) {
         console.error(err);
       }
@@ -77,11 +78,12 @@ window.orbit = () => ({
     const currentWorkspace = this.workspaces.filter(
       (item) => item.attributes.slug === this.selectedWorkspaceSlug
     )[0];
-    const allRepoIdsForCurrentWorkspace = currentWorkspace.relationships.repositories.data.reduce(
-      (repoIdsAccumulator, repositoryData) =>
-        (repoIdsAccumulator = repoIdsAccumulator.concat([repositoryData.id])),
-      []
-    );
+    const allRepoIdsForCurrentWorkspace =
+      currentWorkspace.relationships.repositories.data.reduce(
+        (repoIdsAccumulator, repositoryData) =>
+          (repoIdsAccumulator = repoIdsAccumulator.concat([repositoryData.id])),
+        []
+      );
     const result = allRepoIdsForCurrentWorkspace.map(
       (repositoryId) =>
         this.repositories.filter(
@@ -90,20 +92,66 @@ window.orbit = () => ({
     );
     return result;
   },
+  _persistRepositories(workspaceSlug, repositories) {
+    // Addresses of the chunked repositories so we can retrieve them later
+    let repository_keys = [];
+
+    // Used to number the chunked repository keys
+    let j = 1;
+
+    // Break repositories down into chunks of 100 &
+    // store them separately
+    // IE if there are 240 repositories, they will be saved as:
+    //
+    // sally:repositories:1 // Repositories 0-100
+    // sally:repositories:2 // Repositories 101-200
+    // sally:repositories:3 // Repositories 201-240
+    while (repositories.length > 0) {
+      let key = `${workspaceSlug}:repositories:${j}`;
+
+      repository_keys.push(key);
+      chrome.storage.sync.set({
+        [key]: repositories.splice(0, 100),
+      });
+      j++;
+    }
+
+    return repository_keys;
+  },
+  _buildStorageObject(token, workspaceSlug, repositories) {
+    const storageObject = {};
+
+    // Set common variables
+    storageObject["token"] = token;
+    storageObject["workspace"] = workspaceSlug;
+    storageObject["repository_keys"] = this._persistRepositories(
+      workspaceSlug,
+      repositories
+    );
+
+    return storageObject;
+  },
   save() {
     let that = this;
-    let repositoriesFullNameForWorkspace = this._findAllReposFullNameByWorkspaceSlug();
-    chrome.storage.sync.set(
-      {
-        token: this.token,
-        workspace: this.selectedWorkspaceSlug,
-        repositories: repositoriesFullNameForWorkspace,
-      },
-      function () {
-        that.saveStatus.success = true;
-        that.saveStatus.message =
-          "Saved successfully, you can close this page.";
-      }
+    let repositoriesFullNameForWorkspace =
+      this._findAllReposFullNameByWorkspaceSlug();
+
+    let storageData = this._buildStorageObject(
+      this.token,
+      this.selectedWorkspaceSlug,
+      repositoriesFullNameForWorkspace
     );
+
+    chrome.storage.sync.set(storageData, function () {
+      that.saveStatus.success = true;
+      that.saveStatus.message = "Saved successfully, you can close this page.";
+    });
+
+    // TESTING: Retrieving & logging the stored data
+    chrome.storage.sync.get("repository_keys", ({ repository_keys }) => {
+      chrome.storage.sync.get(repository_keys[0], (repositories) => {
+        console.log("Repositories", repositories);
+      });
+    });
   },
 });
