bde6d75f6cf583f954f798bbf3b32617f971adc7
delete-44 <delete44+tech@protonmail.com>
2023-04-21 16:12:02 +0100

    Uploading the "horrendous" code as a baseline

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,11 @@
 export const ORBIT_API_ROOT_URL = "https://app.orbit.love";
 
 /**
+ * Client ID of the OAUTH application generated in Orbit app
+ */
+export const OAUTH_CLIENT_ID = "xxx";
+
+/**
  * Headers common to all API calls.
  */
 export const ORBIT_HEADERS = {
--- src/options/options.js
+++ src/options/options.js
@@ -1,7 +1,11 @@
 import "chrome-extension-async";
 import Alpine from "alpinejs";
 
-import { ORBIT_API_ROOT_URL, ORBIT_HEADERS } from "../constants";
+import {
+  ORBIT_API_ROOT_URL,
+  OAUTH_CLIENT_ID,
+  ORBIT_HEADERS,
+} from "../constants";
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("orbit", () => ({
@@ -113,6 +117,148 @@ document.addEventListener("alpine:init", () => {
             "Saved successfully, you can close this page.";
         }
       );
+    },
+    async startOAuthFlow(callback) {
+      function parse(str) {
+        if (typeof str !== "string") {
+          return {};
+        }
+        str = str.trim().replace(/^(\?|#|&)/, "");
+        if (!str) {
+          return {};
+        }
+        return str.split("&").reduce(function (ret, param) {
+          var parts = param.replace(/\+/g, " ").split("=");
+          // Firefox (pre 40) decodes `%3D` to `=`
+          // https://github.com/sindresorhus/query-string/pull/37
+          var key = parts.shift();
+          var val = parts.length > 0 ? parts.join("=") : undefined;
+          key = decodeURIComponent(key);
+          // missing `=` should be `null`:
+          // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+          val = val === undefined ? null : decodeURIComponent(val);
+          if (!ret.hasOwnProperty(key)) {
+            ret[key] = val;
+          } else if (Array.isArray(ret[key])) {
+            ret[key].push(val);
+          } else {
+            ret[key] = [ret[key], val];
+          }
+          return ret;
+        }, {});
+      }
+
+      let config = {
+        implicitGrantUrl: "http://localhost:3000/oauth/authorize",
+        clientId: OAUTH_CLIENT_ID,
+        responseType: "code",
+        scopes: "",
+        codeChallengeMethod: "S256",
+      };
+
+      console.log(chrome.identity.getRedirectURL("oauth2"));
+
+      let codeVerifier = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+      async function digestMessage(message) {
+        const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+        const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+        let base64String = encodeURI(
+          btoa(String.fromCharCode(...new Uint8Array(hashBuffer)))
+        );
+        base64String = base64String.split("=")[0];
+        base64String = base64String.replace("+", "-");
+        base64String = base64String.replace("/", "_");
+
+        return base64String;
+      }
+
+      let codeChallenge = await digestMessage(codeVerifier);
+      console.log({ codeChallenge });
+
+      let authUrl =
+        config.implicitGrantUrl +
+        "?response_type=" +
+        config.responseType +
+        "&client_id=" +
+        config.clientId +
+        "&scope=" +
+        config.scopes +
+        "&redirect_uri=" +
+        chrome.identity.getRedirectURL("oauth2") +
+        "&code_challenge=" +
+        codeChallenge +
+        "&code_challenge_method=" +
+        config.codeChallengeMethod;
+
+      console.debug("launchWebAuthFlow:", authUrl);
+
+      chrome.identity.launchWebAuthFlow(
+        { url: authUrl, interactive: true },
+        function (redirectUrl) {
+          if (redirectUrl) {
+            console.debug("launchWebAuthFlow login successful: ", redirectUrl);
+            var parsed = parse(
+              redirectUrl.substr(
+                chrome.identity.getRedirectURL("oauth2").length + 1
+              )
+            );
+            console.debug(parsed);
+
+            console.debug("Background login complete");
+            return callback(parsed.code, codeVerifier); // call the original callback now that we've intercepted what we needed
+          } else {
+            console.debug(
+              "launchWebAuthFlow login failed. Is your redirect URL (" +
+                chrome.identity.getRedirectURL("oauth2") +
+                ") configured with your OAuth2 provider?"
+            );
+            return null;
+          }
+        }
+      );
+    },
+    async getOAuthToken(oAuthCode, codeVerifier) {
+      console.log("in getOAuthToken");
+      console.log({ codeVerifier });
+      console.log({ oAuthCode });
+
+      let config = {
+        implicitGrantUrl: "http://localhost:3000/oauth/token",
+        clientId: OAUTH_CLIENT_ID,
+        grantType: "authorization_code",
+      };
+
+      let authUrl =
+        config.implicitGrantUrl +
+        "?grant_type=" +
+        config.grantType +
+        "&client_id=" +
+        config.clientId +
+        "&code=" +
+        oAuthCode +
+        "&code_verifier=" +
+        codeVerifier +
+        "&redirect_uri=" +
+        chrome.identity.getRedirectURL("oauth2");
+
+      try {
+        const response = await fetch(authUrl, {
+          method: "POST",
+        });
+        const { access_token, refresh_token } = await response.json();
+        chrome.storage.sync.set({
+          accessToken: access_token,
+          refreshToken: refresh_token,
+        });
+        const items = await chrome.storage.sync.get({
+          accessToken: "",
+          refreshToken: "",
+        });
+        console.log(items);
+      } catch (err) {
+        console.error(err);
+      }
     },
   }));
 });
