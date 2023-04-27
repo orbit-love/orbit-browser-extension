9be93df50cd5d97ca865d051d45bca1cc280d3f1
delete-44 <delete44+tech@protonmail.com>
2023-04-27 15:17:23 +0100

    Added tests for function to build request auth

---
+++ src/constants.test.js
@@ -0,0 +1,57 @@
+import { configureRequest, ORBIT_HEADERS } from "./constants";
+
+test("configureRequest should use the OAuth token and not the API key if it is present", () => {
+  const ORBIT_CREDENTIALS = {
+    ACCESS_TOKEN: "123",
+  };
+
+  const testParams = {
+    additionalParam: "789",
+  };
+
+  const testHeaders = {
+    additionalHeader: "456",
+  };
+
+  const { headers, params } = configureRequest(
+    ORBIT_CREDENTIALS,
+    testParams,
+    testHeaders
+  );
+
+  expect(headers).toEqual({
+    ...ORBIT_HEADERS,
+    ...testHeaders,
+    Authorization: "Bearer 123",
+  });
+
+  expect(params.toString()).toMatch("additionalParam=789");
+});
+
+test("configureRequest should use the API key if OAuth token is not present", () => {
+  const ORBIT_CREDENTIALS = {
+    API_TOKEN: "123",
+  };
+
+  const testParams = {
+    additionalParam: "789",
+  };
+
+  const testHeaders = {
+    additionalHeader: "456",
+  };
+
+  const { headers, params } = configureRequest(
+    ORBIT_CREDENTIALS,
+    testParams,
+    testHeaders
+  );
+
+  expect(headers).toEqual({
+    ...ORBIT_HEADERS,
+    ...testHeaders,
+  });
+
+  expect(params.toString()).toMatch("additionalParam=789");
+  expect(params.toString()).toMatch("api_key=123");
+});
