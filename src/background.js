5bbcb46ab76f77131491297325231d0db5ab546c
Nicolas Goutay <nicolas.goutay@gmail.com>
2023-04-27 10:08:44 +0200

    Open options page on install + action button click

---
+++ src/background.js
@@ -0,0 +1,12 @@
+// When clicking on the Orbit extension button, open the options page
+chrome.browserAction.onClicked.addListener(() => chrome.runtime.openOptionsPage());
+
+const isFirstInstall = async (suggestedReason) => suggestedReason === 'install'
+
+// When installing the Orbit extension, open the options page
+chrome.runtime.onInstalled.addListener(async ({reason}) => {
+	// Only notify on install
+	if (await isFirstInstall(reason)) {
+		chrome.runtime.openOptionsPage()
+	}
+});
