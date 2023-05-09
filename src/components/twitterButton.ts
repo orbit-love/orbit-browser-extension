051bb6c606d9fba79499e4264dd5580d256f8a30
delete-44 <delete44+tech@protonmail.com>
2023-05-09 14:22:34 +0100

    Supported dark mode for twitter

--- src/components/twitterButton.ts
+++ src/components/twitterButton.ts
@@ -1,20 +1,26 @@
-import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
-import { unsafeSVG } from 'lit/directives/unsafe-svg';
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { unsafeSVG } from "lit/directives/unsafe-svg";
 import { TailwindMixin } from "../utils/tailwindMixin";
 
-import orbitLogo from 'bundle-text:../icons/orbit-logo.svg'
+import orbitLogo from "bundle-text:../icons/orbit-logo.svg";
 
-@customElement('obe-twitter-button')
+@customElement("obe-twitter-button")
 class TwitterButton extends TailwindMixin(LitElement) {
   render() {
     return html`
       <div>
-        <button type="button" class="min-w-[36px] min-h-[36px] mr-[8px] mb-[12px] text-[#0f1419] hover:bg-[#0f14191A] rounded-full border border-[#cfd9de] flex justify-center items-center" id="menu-button" aria-expanded="true" aria-haspopup="true">
+        <button
+          type="button"
+          class="min-w-[36px] min-h-[36px] mr-[8px] mb-[12px] text-[#0f1419] dark:text-[#eff3f4] hover:bg-[#0f14191A] rounded-full border border-[#cfd9de] dark:border-[#536471] flex justify-center items-center"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
           <span class="sr-only">Open options</span>
           ${unsafeSVG(orbitLogo)}
         </button>
       </div>
-    `
+    `;
   }
 }
